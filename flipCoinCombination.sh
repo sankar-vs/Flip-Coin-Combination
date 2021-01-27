56025812b31c14c5acbe8143a9a576c57e6a8403
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-01-27 09:45:19 +0530

    ADD.Store the result in a Dictionary and finally determine the percentage of the singlet

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,2 +1,21 @@
-#!bin/bash -x
-echo "Welcome to Flip Coin Combination Program"
+#!/bin/bash -x
+declare -A myDict
+countH=0
+countT=0
+total=5
+for (( count=1 ; count<=$total ; count++ ))
+do
+        coinFlip=$((RANDOM %2))
+        if (( ($coinFlip==0) )); then
+                echo "Tails"
+		myDict[$count]=T
+		countT=$((countT+1))
+	else
+                echo "Heads"
+		myDict[$count]=H
+		countH=$((countH+1))
+        fi
+done
+echo ${myDict[@]}
+echo "Percentage of Tails: "`awk 'BEGIN{print '$countT'/'$total'}'`	#$(( $countT/$total ))
+echo "Percentage of Heads: "`awk 'BEGIN{print '$countH'/'$total'}'`	#$(( $countH/$total ))
