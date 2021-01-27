d26005f546d70fafc6c24aa47ce0efa57442a3de
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-01-27 09:58:20 +0530

    ADD.Loop to creat a doublet Combination. Store then and calculate its percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,21 +1,41 @@
 #!/bin/bash -x
 declare -A myDict
-countH=0
-countT=0
-total=5
+countHH=0
+countHT=0
+countTT=0
+countTH=0
+total=10
 for (( count=1 ; count<=$total ; count++ ))
 do
-        coinFlip=$((RANDOM %2))
-        if (( ($coinFlip==0) )); then
-                echo "Tails"
-		myDict[$count]=T
-		countT=$((countT+1))
-	else
-                echo "Heads"
-		myDict[$count]=H
-		countH=$((countH+1))
-        fi
+        coinFlip1=$((RANDOM %2))
+	coinFlip2=$((RANDOM %2))
+	case $coinFlip1 in
+	0)
+		case $coinFlip2 in
+		0)
+			echo "TT"
+			myDict[$count]="TT"
+			(( countTT++ ));;
+		*)
+			echo "TH"
+			myDict[$count]="TH"
+			(( countTH++ ));;
+		esac;;
+	*)
+		case $coinFlip2 in
+		0)
+			echo "HT"
+			myDict[$count]="HT"
+			(( countHT++ ));;
+		*)
+			echo "HH"
+			myDict[$count]="HH"
+			(( countHH++ ));;
+		esac;;
+	esac
 done
 echo ${myDict[@]}
-echo "Percentage of Tails: "`awk 'BEGIN{print '$countT'/'$total'}'`	#$(( $countT/$total ))
-echo "Percentage of Heads: "`awk 'BEGIN{print '$countH'/'$total'}'`	#$(( $countH/$total ))
+echo "Percentage of TT: "`awk 'BEGIN{print '$countTT'/'$total'}'`	#$(( $countTT/$total ))
+echo "Percentage of HH: "`awk 'BEGIN{print '$countHH'/'$total'}'`	#$(( $countHH/$total ))
+echo "Percentage of HT: "`awk 'BEGIN{print '$countHT'/'$total'}'`       #$(( $countHT/$total ))
+echo "Percentage of TH: "`awk 'BEGIN{print '$countTH'/'$total'}'`       #$(( $countTH/$total ))
