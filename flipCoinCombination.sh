2cdc7f2989c475ebc341f123af033dbf8b07cf49
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-01-27 16:32:47 +0530

    ADD.Recode UC3 to have a triplet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,41 +1,78 @@
 #!/bin/bash -x
 declare -A myDict
-countHH=0
-countHT=0
-countTT=0
-countTH=0
+countHHH=0
+countHHT=0
+countHTT=0
+countHTH=0
+countTTT=0
+countTTH=0
+countTHT=0
+countTHH=0
 total=10
 for (( count=1 ; count<=$total ; count++ ))
 do
         coinFlip1=$((RANDOM %2))
 	coinFlip2=$((RANDOM %2))
+	coinFlip3=$((RANDOM %2))
 	case $coinFlip1 in
 	0)
 		case $coinFlip2 in
 		0)
-			echo "TT"
-			myDict[$count]="TT"
-			(( countTT++ ));;
+			case $coinflip3 in
+			0)
+				echo "TTT"
+				myDict[$count]="TTT"
+				(( countTTT++ ));;
+			*)
+				echo "TTH"
+				myDict[$count]="TTH"
+				(( countTTH++ ));;
+			esac;;
 		*)
-			echo "TH"
-			myDict[$count]="TH"
-			(( countTH++ ));;
+			case $coinflip3 in
+                        0)
+                                echo "THT"
+                                myDict[$count]="THT"
+                                (( countTHT++ ));;
+                        *)
+                                echo "THH"
+                                myDict[$count]="THH"
+                                (( countTHH++ ));;
+                        esac;;
 		esac;;
 	*)
 		case $coinFlip2 in
-		0)
-			echo "HT"
-			myDict[$count]="HT"
-			(( countHT++ ));;
-		*)
-			echo "HH"
-			myDict[$count]="HH"
-			(( countHH++ ));;
-		esac;;
+                0)
+                        case $coinflip3 in
+                        0)
+                                echo "HTT"
+                                myDict[$count]="HTT"
+                                (( countHTT++ ));;
+                        *)
+                                echo "HTH"
+                                myDict[$count]="HTH"
+                                (( countHTH++ ));;
+                        esac;;
+                *)
+                        case $coinflip3 in
+                        0)
+                                echo "HHT"
+                                myDict[$count]="HHT"
+                                (( countHHT++ ));;
+                        *)
+                                echo "HHH"
+                                myDict[$count]="HHH"
+                                (( countTHH++ ));;
+                        esac;;
+                esac;;
 	esac
 done
 echo ${myDict[@]}
-echo "Percentage of TT: "`awk 'BEGIN{print '$countTT'/'$total'}'`	#$(( $countTT/$total ))
-echo "Percentage of HH: "`awk 'BEGIN{print '$countHH'/'$total'}'`	#$(( $countHH/$total ))
-echo "Percentage of HT: "`awk 'BEGIN{print '$countHT'/'$total'}'`       #$(( $countHT/$total ))
-echo "Percentage of TH: "`awk 'BEGIN{print '$countTH'/'$total'}'`       #$(( $countTH/$total ))
+echo "Percentage of HTT: "`awk 'BEGIN{print '$countHTT'/'$total'}'`	#$(( $countHTT/$total ))
+echo "Percentage of HHH: "`awk 'BEGIN{print '$countHHH'/'$total'}'`	#$(( $countHHH/$total ))
+echo "Percentage of HHT: "`awk 'BEGIN{print '$countHHT'/'$total'}'`     #$(( $countHHT/$total ))
+echo "Percentage of HTH: "`awk 'BEGIN{print '$countHTH'/'$total'}'`     #$(( $countHTH/$total ))
+echo "Percentage of TTT: "`awk 'BEGIN{print '$countTTT'/'$total'}'`     #$(( $countTTT/$total ))
+echo "Percentage of THH: "`awk 'BEGIN{print '$countTHH'/'$total'}'`     #$(( $countTHH/$total ))
+echo "Percentage of THT: "`awk 'BEGIN{print '$countTHT'/'$total'}'`     #$(( $countTHT/$total ))
+echo "Percentage of TTH: "`awk 'BEGIN{print '$countTTH'/'$total'}'`     #$(( $countTTH/$total ))
